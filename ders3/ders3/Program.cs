8ede784beadf56c3ffcd189618bc0da3b0c42e74
Dessasin <dessasin@pop-os.localdomain>
2019-03-11 10:05:03 +0300

    Tür dönüşümleri ve bellek depolama konumları konusu ile 3. ders devam ediyor

---
+++ ders3/ders3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ders3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+            // /* */ ARASINDA KALAN KISIMLARI /* */ İFADELERİNİ SİLEREK ÇALIŞTIRABİLİRSİNİZ
+        {
+            /* 
+
+            int sayi = 0; // stack bölgesinde yer alan değişkendir
+            const int sayi2 = 0;// sabit ram bölgesinde yer alır ayrıca sabit değişken tanımlama biçimidir 
+
+            {
+                int sayi3 = 12; // ayrılmış blok
+            }
+
+           
+            */
+
+
+            /*
+            byte a = 25;
+            int b = a;
+            Console.WriteLine(b);
+            */
+
+            /*
+            int a = 258; // zorla dönüştürme 
+            byte b = (byte)a;
+            Console.WriteLine(b); // çıktısı "2" dir.
+            */
+
+            /*
+            int a = 258; 
+            checked                // taşma kontrolcüsü
+            {
+                byte b = (byte)a;
+                Console.WriteLine(b);
+            }
+            */
+
+            /*
+            int a = 258;
+            unchecked                // taşmayı göz ardı edilmesi durumudur            
+            {
+                byte b = (byte)a;
+                Console.WriteLine(b);
+            }
+            */
+
+
+            // Tür Dönüşümleri
+
+
+            int sayi1 = 12;
+            sayi1 = Convert.ToByte(sayi1);
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ notlar.md
@@ -0,0 +1,31 @@
+# Notlar 
+
+## Bellek Bölgeleri
+1. Stack (değer tip , değişkenler)
+2. Heap (referans tip,Türetilen nesnelerin saklandığı alan)
+3. Register ( micro işlemci üzerinde bulunan bellek bölgeleri,L1-L2 )
+4. Static (Tüm nesnelerin saklandığı alan)
+5. Sabit (program başında tanımlanıp hiç değişmeyen nesneleri tutan bölge,sabit değişkenler)
+6. Ram olmayan bölge ( Hdd yada Ssd saklama alanı)
+
+---
+### Sabitler 
+* Sabitlere atama yaparken ilk değer verilmek zorundadır
+* Sabitler sonradan değiştirilemez
+* Sabitlere değer atanırken sadece farklı sabitler kullanılabilir
+
+### Genel Uyarılar
+* Kod bloklarını { } ifadeleri ile istediğimiz gibi ayrıştırabiliriz.Ancak dikkat edilmesi geren bir nokta bulunmaktadır;
+ "Değişkenler tanımlı oldukları kod blokları içerisinde geçerli olur".
+
+* **Checked** blokları tür dönüşümlerinde yapılan taşma durumlarını kontrol etmek için kulanılır eğer taşma durumu varsa program hata verip çalışmaz ancak taşma olmadığı durumlarda program çalışacaktır varsayılan olarak **Unchecked**'dir.
+
+* Checked ve Unchecked iç içe kullanılabilir
+
+
+**Objeler** 
+Boxing yöntemi ile kullanılan değişkendir oluşturulan değişkenin türü belirtilmez ancak içeriğine verilen değer değişkenin değer tipini belirler 
+
+### Tür Dönüşümleri
+* Tür dönüşümlerinde değer taşmaları sebebi ile değişken değer hataları oluşabilir
+* Herhangi bir veri tipinde char veri tipine dönüşüm mümkün değildir
